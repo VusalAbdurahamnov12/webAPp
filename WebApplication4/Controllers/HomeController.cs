323f0d3c82691050b01b3556a5fdbd92c568d3db
test <sh13ld001@gmail.com>
2022-05-23 12:56:52 +0400

    first commit

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication4.DAL;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _contexct { get;  }
+        public HomeController(AppDbContext context)
+        {
+            _contexct = context;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _contexct.Sliders.ToList();
+            return View(sliders);
+        }
+
+    }
+}
---
+++ WebApplication4/Models/Slider.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication4.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string SliderTitle { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
